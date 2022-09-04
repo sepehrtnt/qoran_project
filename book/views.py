477c0a4ac45e75a1213e5611f59ba0a3f5c31486
sepehrtnt <tntbordbar@gmail.com>
2022-09-04 13:13:48 +0430

    version 3

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Verses
+from django.db.models import Q
 
 
 def index(request):
@@ -8,15 +9,24 @@ def index(request):
         number_aye = request.POST['number_aye']
         topic = request.POST['topic']
         try:
-            data = Verses.objects.raw(
-                '''SELECT
-                    * 
-                FROM
-                    book_verses 
-                WHERE
-                    page = %s 
-                    and number_aye = %s 
-                    and topic = %s  ''', [page, number_aye, topic])[0]
+            print('*' * 10)
+            # data = Verses.objects.raw(
+            #     '''SELECT
+            #         *
+            #     FROM
+            #         book_verses
+            #     WHERE
+            #         page = %s
+            #         and number_aye = %s
+            #         and topic = %s  ''', [page, number_aye, topic])[0]
+            # return render(request, 'book_search.html',
+            #               {
+            #                   'text': data.text
+            #               })
+            data = Verses.objects.all().filter(page=page, number_aye=number_aye).get()
+            # data = Verses.objects.get(page=page, number_aye=number_aye, topic=topic)
+            print('1' * 10)
+            print(data.text)
             return render(request, 'book_search.html',
                           {
                               'text': data.text
@@ -27,7 +37,7 @@ def index(request):
                               'text': 'چیزی پیدا نشد لطفا دوباره سرچ کنید...'
                           })
 
-    data = Verses.objects.raw('select * from book_verses where page = 1')[0]
+    data = Verses.objects.all().filter(page='2')
     return render(request, 'book_search.html',
                   {
                       'text': data.text
